f90c5ef400b85650211557e786d192b3ca0800ba
AndTheDaysGoBy <afink3@wisc.edu>
2018-01-10 15:08:12 -0600

    Adjusted the Main Window
    Turned the Main Window into a function to be called from main().

--- R-Scripts/Job-Search-Filter-A/Main_Window.R
+++ R-Scripts/Job-Search-Filter-A/Main_Window.R
@@ -1,97 +1,89 @@
-#USER PRESETS
-QUERY <- NULL
-MAXJOBS <- 900
+openMainWindow() <- function() {
+	
+	#Top-Level design
+	mainWindow <- gtkWindow(show=F)
+	mainWindow$setTitle("Job Search Filter")
+	mainWindow$setDefaultSize(600,400)
 
-#GLOBALS
-FILTERS <- matrix(nrow = 0, ncol= 3)
-colnames(FILTERS) <- c("class", "type", "condition")
-DF <- NULL
-TERMS <- array(list(c(),c()))
-dimnames(TERMS) <- list(c('want', 'dont'))
+	#Window layout
+	all <- gtkVBox()
+	mainWindow$add(all)
 
-#Top-Level design
-mainWindow <- gtkWindow(show=F)
-mainWindow$setTitle("Job Search Filter")
-mainWindow$setDefaultSize(600,400)
+	#Window design
+	menu <- gtkMenuBar()
+	core <- gtkHBox(homogeneous=T, spacing=10)
+	all$packStart(menu, expand=F, fill=F)
+	all$packStart(core)
 
-#Window layout
-all <- gtkVBox()
-mainWindow$add(all)
+	#Menubar
+	mFile <- gtkMenuItemNewWithLabel("File", show = T)
+	mConstraints <- gtkMenuItemNewWithLabel("Constraints", show=T)
+	menu$add(mfile)
+	menu$add(mconstraints)
 
-#Window design
-menu <- gtkMenuBar()
-core <- gtkHBox(homogeneous=T, spacing=10)
-all$packStart(menu, expand=F, fill=F)
-all$packStart(core)
+	#File submenu
+	subFile <- gtkMenu()
+	mFile$setSubmenu(subFile)
 
-#Menubar
-mFile <- gtkMenuItemNewWithLabel("File", show = T)
-mConstraints <- gtkMenuItemNewWithLabel("Constraints", show=T)
-menu$add(mfile)
-menu$add(mconstraints)
+	#File submenu:pull item
+	mPull <- gtkMenuItemNewWithLabel("Pull", show = T)
+	gSignalConnect(mPull, "activate", f=function(a, data) {
+		pull(NULL)
+	})
+	subFile$append(mPull)
 
-#File submenu
-subFile <- gtkMenu()
-mFile$setSubmenu(subFile)
+	#File submenu:save item
+	mSave <- gtkMenuItemNewWithLabel("Save", show=T)
+	gSignalConnect(mSave, "active", f=function(a, data) {
+		save()
+	})
+	subFile$append(mSave)
 
-#File submenu:pull item
-mPull <- gtkMenuItemNewWithLabel("Pull", show = T)
-gSignalConnect(mPull, "activate", f=function(a, data) {
-	pull(NULL)
-})
-subFile$append(mPull)
+	#File submenu:load item
+	mLoad <- gtkMenuItemNewWithLabel("Load", show=T)
+	gSignalConnect(mLoad, "active", f=function(a, data) {
+		load()
+	})
+	subFile$append(mLoad)
 
-#File submenu:save item
-mSave <- gtkMenuItemNewWithLabel("Save", show=T)
-gSignalConnect(mSave, "active", f=function(a, data) {
-	save()
-})
-subFile$append(mSave)
+	#Constraints submenu
+	subConstraints <- gtkMenu()
+	mConstraints$setSubmenu(subConstraints)
 
-#File submenu:load item
-mLoad <- gtkMenuItemNewWithLabel("Load", show=T)
-gSignalConnect(mLoad, "active", f=function(a, data) {
-  load()
-})
-subFile$append(mLoad)
+	#Constraints submenu:keyword item
+	mKeywords <- gtkMenuItemNewWithLabel("Keywords", show=T)
+	gSignalConnect(mKeywords, "activate", f=function(a, data) {
+		openKeywordWindow()
+	})
+	subConstraints$append(mKeywords)
 
-#Constraints submenu
-subConstraints <- gtkMenu()
-mConstraints$setSubmenu(subConstraints)
+	mFilters <- gtkMenuItemNewWithLabel("Filter(s)", show=T)
+	gSignalConnect(mFilters, "activate", f=function(a, data) {
+		openFilterWindow()
+	})
+	subConstraints$append(mFilters)
 
-#Constraints submenu:keyword item
-mKeywords <- gtkMenuItemNewWithLabel("Keywords", show=T)
-gSignalConnect(mKeywords, "activate", f=function(a, data) {
-	openKeywordWindow()
-})
-subConstraints$append(mKeywords)
+	#Core
+	jobList <- gtkFrame("Job List")
+	jobPage <- gtkFrame("Job Page")
+	core$packStart(jobList, fill=T)
+	core$packStart(jobPage, fill=T)
 
-mFilters <- gtkMenuItemNewWithLabel("Filter(s)", show=T)
-gSignalConnect(mFilters, "activate", f=function(a, data) {
-	openFilterWindow()
-})
-subConstraints$append(mFilters)
+	#jobList
+	scrollJobPane <- gtkScrolledWindow()
+	scrollJobPane$setPolicy("never", "automatic")
+	jobList$add(scrollJobPane)
 
-#Core
-jobList <- gtkFrame("Job List")
-jobPage <- gtkFrame("Job Page")
-core$packStart(jobList, fill=T)
-core$packStart(jobPage, fill=T)
+	#scrollJobPane
+	renderedJobs <- gtkVBox(homogeneous=T, spacing=5)
+	scrolled$addWithViewport(renderedJobs)
 
-#jobList
-scrollJobPane <- gtkScrolledWindow()
-scrollJobPane$setPolicy("never", "automatic")
-jobList$add(scrollJobPane)
+	#jobPage
+	notebook <- gtkNotebook()
+	notebook$appendPage(gtkLabel("Page 1"), gtkLabel("Tab 1"))
+	notebook$appendPage(gtkLabel("Page 2"), gtkLabel("Tab 2"))
+	jobPage$add(notebook)
 
-#scrollJobPane
-renderedJobs <- gtkVBox(homogeneous=T, spacing=5)
-scrolled$addWithViewport(renderedJobs)
-
-#jobPage
-notebook <- gtkNotebook()
-notebook$appendPage(gtkLabel("Page 1"), gtkLabel("Tab 1"))
-notebook$appendPage(gtkLabel("Page 2"), gtkLabel("Tab 2"))
-jobPage$add(notebook)
-
-#Display
-mainWindow$show()
+	#Display
+	mainWindow$show()
+}
